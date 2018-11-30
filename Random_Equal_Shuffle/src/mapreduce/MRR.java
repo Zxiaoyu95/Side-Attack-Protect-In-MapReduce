f8938163c0e21631ecfebc69a24b0ae94633b0b6
Zxiaoyu95 <884823258@qq.com>
2018-11-30 22:20:25 +0800

    加密之后

--- Random_Equal_Shuffle/src/mapreduce/MRR.java
+++ Random_Equal_Shuffle/src/mapreduce/MRR.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.jobcontrol.JobControl;
+//import org.apache.hadoop.mapred.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,13 +17,12 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 public class MRR {
-	static int numReduceTasks =3;
+	static int numReduceTasks =7;
 /*job1*/
 	 public static class MyMapper extends Mapper<LongWritable,Text,Text,Text>{
 		@Override
 		protected void setup(Mapper<LongWritable, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
-			// TODO Auto-generated method stub
 			super.setup(context);
 		}
 		@Override
@@ -31,7 +30,7 @@ public class MRR {
 				throws IOException, InterruptedException {
 			String valueStr=value.toString();
 			String [] values=valueStr.split("	");
-			context.write(new Text(values[0].replace("\"", "")), new Text("1"));
+			context.write(new Text(values[5].replace("\"", "")), new Text("1"));
 			
 		}
 	}
@@ -130,9 +129,10 @@ public class MRR {
     	//获取配置对象信息
     	Configuration conf = new Configuration();
 //job1设置 	
-    	Job job1 =Job.getInstance(conf,"job1");
+//    	Job job1 =Job.getInstance(conf,"job1"); 
+    	Job job1 =new Job();
     	//设置job的运行主类
-    	job1.setJarByClass(Partition.class);
+    	job1.setJarByClass(MRR.class);
     	FileInputFormat.setInputPaths(job1, new Path(args[0]));
     	//对map阶段进行设置
     	job1.setMapperClass(MyMapper.class);
@@ -144,7 +144,7 @@ public class MRR {
     	//对reduce阶段设置、
     	job1.setReducerClass(ShuffleReduce.class);
     	job1.setOutputKeyClass(Text.class);
-    	job1.setNumReduceTasks(3);//reduce 数量设定
+    	job1.setNumReduceTasks(7);//reduce 数量设定
     	job1.setOutputValueClass(Text.class);
     	//提交job
     	FileOutputFormat.setOutputPath(job1, new Path(args[1]));
@@ -152,9 +152,10 @@ public class MRR {
         ControlledJob ctrlJob1= new ControlledJob(conf);
         ctrlJob1.setJob(job1);
 //job2设置
-        Job job2 =Job.getInstance(conf,"job2");
+//        Job job2 =Job.getInstance(conf,"job2");
+        Job job2 =new Job();
     	//设置job的运行主类
-        job2.setJarByClass(Partition.class);
+        job2.setJarByClass(MRR.class);
     	FileInputFormat.setInputPaths(job2, new Path(args[1]));
     	//对map阶段进行设置
     	job2.setMapperClass(MyMapper2.class);
@@ -165,30 +166,33 @@ public class MRR {
     	//对reduce阶段设置、
     	job2.setReducerClass(ShuffleReduce2.class);
     	job2.setOutputKeyClass(Text.class);
-    	job2.setNumReduceTasks(3);//reduce 数量设定
+    	job2.setNumReduceTasks(7);//reduce 数量设定
     	job2.setOutputValueClass(Text.class);
     	//提交job
     	FileOutputFormat.setOutputPath(job2, new Path(args[2]));
     	//控制器设置
         ControlledJob ctrlJob2= new ControlledJob(conf);
         ctrlJob2.setJob(job2);
- //设置作业之间关系，job2输入job1输出
-        ctrlJob2.addDependingJob(ctrlJob1);
- //设置主控制器，控制job1和job2两个作业
-        JobControl jobCtrl= new JobControl("myCtrl");
-        jobCtrl.addJob(ctrlJob1);
-        jobCtrl.addJob(ctrlJob2);
-    	
-        //在线程中启动
-        Thread thread = new Thread(jobCtrl);
-        thread.start();
-        while (true) {
-            if (jobCtrl.allFinished()) {
-                System.out.println(jobCtrl.getSuccessfulJobList());
-                jobCtrl.stop();
-                break;
-            }
-        }
+// //设置作业之间关系，job2输入job1输出
+//        ctrlJob2.addDependingJob(ctrlJob1);
+// //设置主控制器，控制job1和job2两个作业
+//        JobControl jobCtrl= new JobControl("myCtrl");
+//        jobCtrl.addJob(ctrlJob1);
+//        jobCtrl.addJob(ctrlJob2);
+//    	
+//        //在线程中启动
+//        Thread thread = new Thread(jobCtrl);
+//        thread.start();
+//        while (true) {
+//            if (jobCtrl.allFinished()) {
+//                System.out.println(jobCtrl.getSuccessfulJobList());
+//                jobCtrl.stop();
+//                break;
+//            }
+//        }
+        if (job1.waitForCompletion(true)) {
+                   System.exit(job2.waitForCompletion(true) ? 0 : 1);
+        	       }
     	long endTime=System.currentTimeMillis();
     	System.out.println("运行时间："+(endTime-startTime)+"ms");
     }
--- Random_Equal_Shuffle/src/mapreduce/MapReduce.java
+++ Random_Equal_Shuffle/src/mapreduce/MapReduce.java
@@ -18,28 +18,15 @@ public class MapReduce {
 
 	 public static class MyMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
 		@Override
-		protected void setup(Mapper<LongWritable, Text, Text, IntWritable>.Context context)
-				throws IOException, InterruptedException {
-			// TODO Auto-generated method stub
-			super.setup(context);
-		}
-		@Override
 		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
 			String valueStr=value.toString();
-			String [] values=valueStr.split("	");
-			int r=(values[0].hashCode()&Integer.MAX_VALUE)%3;
-			context.write(new Text(r+"_"+values[0].replace("\"", "")), new IntWritable(1));
+			String [] values=valueStr.split("	");	
+			context.write(new Text(values[5].replace("\"", "")), new IntWritable(1));
 	
 		}
 	}
 	public static class ShuffleReduce extends Reducer<Text,IntWritable,Text,IntWritable>{
-		@Override
-		protected void setup(Reducer<Text, IntWritable, Text, IntWritable>.Context context)
-				throws IOException, InterruptedException {
-			// TODO Auto-generated method stub
-			super.setup(context);
-		}
 		@Override
 		protected void reduce(Text key, Iterable<IntWritable>values,Context context) throws IOException, InterruptedException {
 			int sum=0;
@@ -64,8 +51,8 @@ public class MapReduce {
 	static class MyPartitioner extends HashPartitioner<Text,IntWritable>{
 		@Override
 		public int getPartition(Text key, IntWritable value, int numReduceTasks) {
-			String keystr=key.toString();
-			return Integer.parseInt(keystr.substring(0,keystr.indexOf("_")));
+			
+			return (key.hashCode()&Integer.MAX_VALUE)%numReduceTasks;
 			
 		}
 	}
@@ -73,7 +60,8 @@ public class MapReduce {
     	long startTime=System.currentTimeMillis();
     	//获取配置对象信息
     	Configuration conf = new Configuration();
-    	Job job =Job.getInstance(conf,"mapreduce");
+//    	Job job =Job.getInstance(conf,"mapreduce");
+    	Job job =new Job();
     	//设置job的运行主类
     	job.setJarByClass(MapReduce.class);
     	//对map阶段进行设置
@@ -86,7 +74,7 @@ public class MapReduce {
     	job.setReducerClass(ShuffleReduce.class);
     	job.setPartitionerClass(MyPartitioner.class);
     	job.setOutputKeyClass(Text.class);
-    	job.setNumReduceTasks(3);//reduce 数量设定
+    	job.setNumReduceTasks(7);//reduce 数量设定
     	job.setOutputValueClass(IntWritable.class);
     	FileOutputFormat.setOutputPath(job, new Path(args[1]));
     	//提交job
